ec01a536e96771c03ebf6ea16ac82be5fb86c2b5
Jereco Peralta <peraltajereco@gmail.com>
2026-03-28 11:34:30 +0800

    Implement the Customer Hub

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   /* config options here */
-  allowedDevOrigins: ['msi-andrie', 'msi-4', '100.81.225.79', '100.124.104.46'],
+  allowedDevOrigins: [
+    "msi-andrie",
+    "msi-4",
+    "100.81.225.79",
+    "100.124.104.46",
+    "100.119.3.44",
+    "192.168.0.247",
+    "localhost"
+  ],
 };
 
 export default nextConfig;
