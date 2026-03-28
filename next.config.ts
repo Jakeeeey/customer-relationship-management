42618f6e374435ce0cff71baf5be360200bde22e
Jereco Peralta <peraltajereco@gmail.com>
2026-03-28 11:34:30 +0800

    Implement the Customer Hub

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   /* config options here */
+  allowedDevOrigins: ["100.119.3.44", "192.168.0.247", "100.81.225.79", "localhost"],
+
 };
 
 export default nextConfig;
