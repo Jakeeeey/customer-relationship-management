6041723e031afec27b7c28516dcc1bc7a4d2189c
Jereco Peralta <peraltajereco@gmail.com>
2026-03-28 11:40:43 +0800

    Initial changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["100.119.3.44", "192.168.0.247", "100.81.225.79", "localhost"],
 
 };
 
