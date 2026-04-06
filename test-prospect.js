2d9cc98ff2ee6551eaa32f55723b8de33fa13b42
Xerman09 <ermancerujano10@gmail.com>
2026-04-06 11:30:44 +0800

    New Module Customer Prospecting

---
+++ test-prospect.js
@@ -0,0 +1,29 @@
+const token = 'rTilKSsclzuQW8WfQWK1ba8wrD_LetNn';
+const listUrl = 'http://goatedcodoer:8056/items/customer_prospect?limit=1';
+
+fetch(listUrl, { headers: { Authorization: `Bearer ${token}` } })
+  .then(res => res.json())
+  .then(listData => {
+    if (!listData.data || listData.data.length === 0) {
+      console.log('No prospects found.');
+      process.exit(0);
+    }
+    const prospect = listData.data[0];
+    console.log('Found prospect ID:', prospect.id);
+    
+    const { id, prospect_status, prospect_date, salesman_id, salesman_name, user_id, ...customerData } = prospect;
+    console.log('Customer Data:', customerData);
+    
+    return fetch('http://goatedcodoer:8056/items/customer', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+      body: JSON.stringify({ ...customerData, isActive: 1, user_id: user_id || null })
+    });
+  })
+  .then(async res => {
+    if (!res) return;
+    console.log('Customer creation status:', res.status);
+    console.log('Response:', await res.text());
+  })
+  .catch(err => console.error(err));
+
